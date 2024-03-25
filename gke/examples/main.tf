22ed5bfcdd46c13edd77484f2ab0c99f025f13b7
“Hanna <anna.dzhus@gmail.com>
2024-03-25 17:39:13 -0500

    modules exaples

--- gke/examples/main.tf
+++ gke/examples/main.tf
@@ -11,3 +11,16 @@ module "dev-cluster" {
   disk_size_gb = "size"
   
 }
+
+
+module "qa-cluster" {
+  source = "../"
+  region = "us-central1-c"
+  project_id = "your projet id"
+  service_account_id = "your sa id" 
+  sa_display_name = "service acc for your cluster"
+  cluster_name = "cluster name"
+  location = "any location"
+  initial_node_count = "node numbers"
+  disk_size_gb = "size"
+}
